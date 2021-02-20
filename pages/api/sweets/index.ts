750cc3d6b4413d3c8281e344e26c3f8463e6e7a3
kntowd <kenta2231989@gmail.com>
2021-02-20 23:38:17 +0900

    can get all sweets

--- pages/api/sweets/index.ts
+++ pages/api/sweets/index.ts
@@ -8,6 +8,14 @@ export default async function handler(req, res) {
 
   switch (method) {
     case 'GET':
+      try {
+        /* get all sweets */
+        const sweets = await Sweet.find({});
+        res.status(201).json({ success: true, sweets });
+      }
+      catch (error) {
+        res.status(400).json({ success: false });
+      }
       break;
     case 'POST':
       try {
