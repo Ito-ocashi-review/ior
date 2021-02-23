0d31401b6b517f4d23ba1f54e12fe6ee5aabe9db
kntowd <kenta2231989@gmail.com>
2021-02-23 21:18:21 +0900

    experimental commit

--- pages/api/reviews.ts/index.ts
+++ pages/api/reviews.ts/index.ts
@@ -3,38 +3,38 @@ import dbConnect from '../../../utils/dbConnect';
 
 export default async function handler(req, res) {
   const { method, body } = req;
-
+  console.log(method);
   await dbConnect();
 
-  switch (method) {
-    case 'GET':
-      try {
-        /* get all Reviews */
-        const reviews = await Review.find({});
-        res.status(201).json({ success: true, reviews });
-      }
-      catch (error) {
-        res.status(400).json({ success: false });
-      }
-      break;
-    case 'POST':
-      try {
-        const { data } = body;
-        await Review.create(
-          {
-            sweetId: data.sweetId,
-            comment: data.comment,
-            evaluation: data.evaluation,
-          },
-        ); /* create a new model in the database */
-        res.status(201).json({ success: true, data: Review });
-      }
-      catch (error) {
-        res.status(400).json({ success: false });
-      }
-      break;
-    default:
-      res.status(400).json({ success: false });
-      break;
+  // switch (method) {
+  // case 'GET':
+  //   try {
+  //     /* get all Reviews */
+  //     const reviews = await Review.find({});
+  //     res.status(201).json({ success: true, reviews });
+  //   }
+  //   catch (error) {
+  //     res.status(400).json({ success: false });
+  //   }
+  //   break;
+  // case 'POST':
+  try {
+    const { data } = body;
+    await Review.create(
+      {
+        sweetId: data.sweetId,
+        comment: data.comment,
+        evaluation: data.evaluation,
+      },
+    ); /* create a new model in the database */
+    res.status(201).json({ success: true, data: Review });
   }
+  catch (error) {
+    res.status(400).json({ success: false });
+  }
+  //   break;
+  // default:
+  //   res.status(400).json({ success: false });
+  //   break;
+  // }
 }
