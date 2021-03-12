e5551b0213f37f9d332232bcd921af95cd6248d8
kntowd <kenta2231989@gmail.com>
2021-03-12 23:39:53 +0900

    remove next-auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,8 @@ import {
 import { makeStyles } from '@material-ui/core/styles';
 import withReactContent from 'sweetalert2-react-content';
 import Swal from 'sweetalert2';
-import { useSession } from 'next-auth/client';
 import Router from 'next/router';
 import Axios from 'axios';
-import OAuthButton from '../components/OAuthButton';
 import SweetRanking from '../components/topRanking/SweetRanking';
 import TotalRanking from '../components/totalRanking/TotalRanking';
 import firebase from '../Firebase';
@@ -24,16 +22,6 @@ const handleFirebaseLogout = () => {
     // An error happened.
   });
   console.log('logoutするよ');
-};
-
-const handleLogin = () => {
-  MySwal.fire({
-    title: 'ログインする',
-    html: <OAuthButton />,
-    showConfirmButton: false,
-    showCancelButton: true,
-    cancelButtonColor: '#d33',
-  });
 };
 
 const useStyles = makeStyles(theme => ({
@@ -79,7 +67,6 @@ type Props ={
 }
 
 const Index: React.FC<Props> = ({ sweetsData }) => {
-  const [session, loading] = useSession();
   const [sweetRanking, setsweetRanking] = useState([]);
   const [user, setUser] = useState('ログインユーザはいません');
 
@@ -139,24 +126,14 @@ const Index: React.FC<Props> = ({ sweetsData }) => {
         <button type="button" onClick={() => handleFirebaseLogin()}>firebaseのログイン</button>
         <button type="button" onClick={() => handleFirebaseLogout()}>firebaseのログアウト</button>
       </div>
-      {session && (
-        <Button
-          variant="outlined"
-          className={classes.reviewButton}
-          onClick={() => Router.push('/new-review')}
-        >
-          投稿する
-        </Button>
-        )}
-      {!session && (
-        <Button
-          variant="outlined"
-          className={classes.reviewButton}
-          onClick={() => handleLogin()}
-        >
-          投稿するにはログインが必要です
-        </Button>
-        )}
+
+      <Button
+        variant="outlined"
+        className={classes.reviewButton}
+        onClick={() => Router.push('/new-review')}
+      >
+        投稿する
+      </Button>
     </div>
   );
 };
