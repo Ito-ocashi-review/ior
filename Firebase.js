70ca80519d357f61af7aa1c22e699f7aa0333c1f
kntowd <kenta2231989@gmail.com>
2021-04-04 18:57:10 +0900

    remove hard cord

--- Firebase.js
+++ Firebase.js
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD4wagzTUA_4VDH1D6nKVdcS_dD5vIqrjg',
-  authDomain: 'ito-ocashi-75f9d.firebaseapp.com',
-  databaseURL: 'https://ito-ocashi-75f9d-default-rtdb.firebaseio.com',
-  projectId: 'ito-ocashi-75f9d',
-  storageBucket: 'ito-ocashi-75f9d.appspot.com',
-  messagingSenderId: '965467433859',
-  appId: '1:965467433859:web:3f3ceb847d8c6ca59fa4b6',
-  measurementId: 'G-FPY79Y4H25',
+  apiKey: process.env.NEXT_PUBLIC_FIRE_BASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIRE_BASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIRE_BASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIRE_BASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIRE_BASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIRE_BASE_MESSSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIRE_BASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIRE_BASE_MESUREMENT_ID,
 };
 
 if (firebase.apps.length === 0) {
