8f20aae4e2a6014784608d536047605f219cfcbc
kntowd <kenta2231989@gmail.com>
2021-04-14 22:03:29 +0900

    test commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,106 +1,106 @@
 import React from 'react';
-// import { Button, Grid } from '@material-ui/core';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Router from 'next/router';
-// import Axios from 'axios';
-// import SweetRanking from '../components/topRanking/SweetTopRanking';
-// import TotalRanking from '../components/overalllRanking/OverallRanking';
-// import { AuthContext } from './_app';
+import { Button, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Router from 'next/router';
+import Axios from 'axios';
+import SweetRanking from '../components/topRanking/SweetTopRanking';
+import TotalRanking from '../components/overalllRanking/OverallRanking';
+import { AuthContext } from './_app';
 
-// const useStyles = makeStyles({
-//   top: {
-//     color: '#270000',
-//     height: '100vh',
-//     textAlign: 'center',
-//   },
-//   section: {
-//     margin: '80px 50px',
-//   },
-//   title: {
-//     fontSize: '100px',
-//     font: 'MotoyalMaru',
-//   },
-//   sweetRanking: {
-//     margin: '30px 0',
-//   },
-//   reviewButton: {
-//     color: 'black',
-//     border: '2px solid #984B15',
-//     backgroundColor: '#984B15',
-//     fontWeight: 'bold',
-//     '&:hover': {
-//       backgroundColor: '#984B15',
-//       borderRadius: '30px',
-//       transition: 'all 0.2s linear',
-//     },
-//   },
-//   totalRanking: {
-//     margin: '30px 0',
-//   },
-// });
+const useStyles = makeStyles({
+  top: {
+    color: '#270000',
+    height: '100vh',
+    textAlign: 'center',
+  },
+  section: {
+    margin: '80px 50px',
+  },
+  title: {
+    fontSize: '100px',
+    font: 'MotoyalMaru',
+  },
+  sweetRanking: {
+    margin: '30px 0',
+  },
+  reviewButton: {
+    color: 'black',
+    border: '2px solid #984B15',
+    backgroundColor: '#984B15',
+    fontWeight: 'bold',
+    '&:hover': {
+      backgroundColor: '#984B15',
+      borderRadius: '30px',
+      transition: 'all 0.2s linear',
+    },
+  },
+  totalRanking: {
+    margin: '30px 0',
+  },
+});
 
-// type data = {
-//   id: string;
-//   name: string;
-//   evaluation: number;
-// };
+type data = {
+  id: string;
+  name: string;
+  evaluation: number;
+};
 
-// type Props = {
-//   sweetsData: data[];
-// };
+type Props = {
+  sweetsData: data[];
+};
 
 const Index: React.FC<Props> = () => {
-  // const [sweetRanking, setsweetRanking] = useState([]);
-  // const classes = useStyles();
-  // const { currentUser, login } = useContext(AuthContext);
-  // const postButton = () => {
-  //   if (currentUser) {
-  //     return (
-  //       <Button
-  //         variant="outlined"
-  //         className={classes.reviewButton}
-  //         onClick={() => Router.push('/new-review')}
-  //       >
-  //         投稿する
-  //       </Button>
-  //     );
-  //   }
-  //   return (
-  //     <Button
-  //       variant="outlined"
-  //       className={classes.reviewButton}
-  //       onClick={() => login()}
-  //     >
-  //       投稿をするにはログインをしてください
-  //     </Button>
-  //   );
-  // };
-  // useEffect(() => {
-  //   const getRanking = async () => {
-  //     const ranking = await Axios.get('/api/sweets/ranking');
-  //     setsweetRanking(ranking.data.sortedSweetsRankingData);
-  //   };
-  //   getRanking();
-  // }, []);
-  // return (
-  //   <div className={classes.top}>
-  //     <div className={classes.section}>
-  //       <div className={classes.sweetRanking}>
-  //         <Grid container spacing={3}>
-  //           <SweetRanking sweetsData={sweetRanking} />
-  //         </Grid>
-  //       </div>
-  //     </div>
-  //     <div className={classes.section}>
-  //       <div className={classes.totalRanking}>
-  //         <Grid container spacing={8}>
-  //           <TotalRanking sweetRanking={sweetRanking} />
-  //         </Grid>
-  //       </div>
-  //     </div>
-  //     {postButton()}
-  //   </div>
-  // );
+  const [sweetRanking, setsweetRanking] = useState([]);
+  const classes = useStyles();
+  const { currentUser, login } = useContext(AuthContext);
+  const postButton = () => {
+    if (currentUser) {
+      return (
+        <Button
+          variant="outlined"
+          className={classes.reviewButton}
+          onClick={() => Router.push('/new-review')}
+        >
+          投稿する
+        </Button>
+      );
+    }
+    return (
+      <Button
+        variant="outlined"
+        className={classes.reviewButton}
+        onClick={() => login()}
+      >
+        投稿をするにはログインをしてください
+      </Button>
+    );
+  };
+  useEffect(() => {
+    const getRanking = async () => {
+      const ranking = await Axios.get('/api/sweets/ranking');
+      setsweetRanking(ranking.data.sortedSweetsRankingData);
+    };
+    getRanking();
+  }, []);
+  return (
+    <div className={classes.top}>
+      <div className={classes.section}>
+        <div className={classes.sweetRanking}>
+          <Grid container spacing={3}>
+            <SweetRanking sweetsData={sweetRanking} />
+          </Grid>
+        </div>
+      </div>
+      <div className={classes.section}>
+        <div className={classes.totalRanking}>
+          <Grid container spacing={8}>
+            <TotalRanking sweetRanking={sweetRanking} />
+          </Grid>
+        </div>
+      </div>
+      {postButton()}
+    </div>
+  );
   <p>fjdskfsdk</p>;
 };
 
