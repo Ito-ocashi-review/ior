6b50ebc6587802cc6be7a0b68e45c5242200aff3
kntowd <kenta2231989@gmail.com>
2021-04-14 22:07:01 +0900

    test commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { Button, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Router from 'next/router';
